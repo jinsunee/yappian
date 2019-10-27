13145f1c77816baa250e078d472845fba878ca45
ilikeu7246 <jsunee7246@gmail.com>
2019-10-27 22:29:29 +0900

    Add esilnt and prettier setting. #4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
-import { MainContainer, UrlContainer, NotFound } from './containers'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MainContainer, UrlContainer, NotFound } from './containers';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Switch>
-                <Route exact path="/" component={MainContainer} />
-                <Route path="/project" component={UrlContainer} />
-                <Route component={NotFound} />
-            </Switch>
-        </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={MainContainer} />
+                    <Route path="/project" component={UrlContainer} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
